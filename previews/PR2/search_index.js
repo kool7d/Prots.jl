15648f0a57527f597bae53bdea3c916073ac3bea
Documenter.jl <documenter@juliadocs.github.io>
2021-07-07 15:46:14 +0000

    build based on 6def605

---
+++ previews/PR2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Prots","category":"page"},{"location":"#Prots","page":"Home","title":"Prots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Prots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Prots]","category":"page"}]
+}
